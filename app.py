774a134d1886774f8caf379725b896e9b0cfda90
Donna Richards <15822195+DonnaRichards@users.noreply.github.com>
2020-07-06 07:52:30 +1000

    Backup of local code updates done over the weekend

--- app.py
+++ app.py
@@ -1,10 +1,35 @@
-# Import your Game class
-from ./phrasehunter/game.py import Game
+from ./phrasehunter/game import Game
+from ./phrasehunter/phraselist import PHRASELIST
 
-# Create your Dunder Main statement.
+"""
+Flow of the game
+
+Using Python, you’ll create three Python classes with specific attributes and methods. 
+You'll create a Game class for managing the game, and a Phrase class to help with storing attributes of a 
+phrase with specific methods to help determine how to display the phrase in the game.
+
+Your code will choose a random phrase and use some logic you will implement to display each letter of the phrase
+ as underscore character placeholders, _.
+
+Each time the player guesses a letter, the program compares the letter the player has chosen with the random phrase. 
+If the letter is in the phrase, the phrase object is updated so that it displays the chosen letters on the screen.
+
+A player continues to select letters until they guess the phrase (and win), or make five incorrect guesses (and lose).
+
+If the player completes the phrase before they run out of guesses, a winning screen appears. 
+If the player guesses incorrectly five times, a losing screen appears.
+
+A player can guess a letter only once. After they’ve guessed a letter, your programming logic will need to 
+prevent that letter from being guessed a 2nd time.
+"""
 if __name__ == "__main__":
 # Inside Dunder Main:
 ## Create an instance of your Game class
 ## Start your game by calling the instance method that starts the game loop
-    game = Game()
-    game.start()
+   play_again = 'y'
+   while play_again.lower() = 'y':
+      game = Game(PHRASELIST)
+      game.start()
+      play_again = input('Do you want to play again ? (y/n) ')
+      del game
+   print('Thank you for playing the game !  Enjoy the rest of your day')
--- phrasehunter/game.py
+++ phrasehunter/game.py
@@ -1,5 +1,90 @@
 # Create your Game class logic in here.
+import random
+import re
+
 class Game():
+    '''
+ |  Class representing a company employee.
+ |  
+ |  Attributes
+ |   ----------
+ |   name : str 
+ |       Employee's name        
+ |   email : str, default None
+ |       Employee's email
+ |   salary : float, default None
+ |       Employee's salary
+ |   rank : int, default 5
+ |       The rank of the employee in the company hierarchy (1 -- CEO, 2 -- direct reports of CEO, 3 -- direct reports of direct reports of CEO etc). Cannot be None if the employee is current.
+ |  
+ |  Methods defined here:
+ |  
+ |  __init__(self, name, email=None, salary=None, rank=5)
+ |      Create an Employee object
+ |  
+ |  give_raise(self, amount)
+ |      Raise employee's salary by a certain `amount`. Can only be used with current employees.
+ |      
+ |      Example usage:
+ |        # emp is an Employee object
+ |        emp.give_raise(1000)
+ |  
+ |  promote(self)
+ |      Promote an employee to the next level of the company hierarchy. Decreases the rank of the employee by 1. Can only be used on current employeed who are not at the top of the hierarchy.
+ |      
+ |      Example usage:
+ |          # emp is an Employee object
+ |          emp.promote()
+ |  
+ |  terminate(self)
+ |      Terminate the employee. Sets salary and rank to None..
+ |      
+ |      Example usage:
+ |         # emp is an Employee object
+ |         emp.terminate()
+ |  
+ |  ----------------------------------------------------------------------
+ |  Data descriptors defined here:
+ |  
+ |  __dict__
+ |      dictionary for instance variables (if defined)
+ |  
+ |  __weakref__
+ |      list of weak references to the object (if defined)
+    '''
+
     # methods for starting the game, handling interactions, getting a random phrase, 
     # checking for a win/loss state, and removing "lives" or turns from the player.
-    pass
+    def __init__(self, phrases):
+        self.phrase = Phrase(phrases[randint(len(phrases))])
+        self.won = False
+        self.num_guesses = 0
+
+    def get_guess(self):
+        valid_guess = False
+        while not valid_guess:
+            try:
+                user_guess = input('Enter a letter (a-z): ')
+                if len(user_guess) > 1:
+                    raise ValueError('Too many characters entered - please enter only one letter')
+                if re.search(user_guess, [^a-z]):
+                    raise ValueError('Invalid character entered - please enter a letter (a-z) ')
+            except ValueError:
+                continue
+            valid_guess = True
+        return user_guess
+
+
+    def start(self):
+        print('Welcome to Phrase Hunter !')
+        print('guess a letter in the phrase')
+        print('5 guesses to get the phrase')
+        print('Your phrase is: ',end='')
+        self.phrase.display_phrase()
+        while not self.won and self.num_guesses < 6:
+            guess = get_guess()
+            good_guess = phrase.check_guess(guess)
+
+
+
+
